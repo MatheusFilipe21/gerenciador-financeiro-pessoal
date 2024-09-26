25048d28cd5037fb5df4f91c88402434e3cfa57a
MatheusFilipe21 <matheusfnpereira@gmail.com>
2024-09-26 14:32:43 -0300

    Criando rota atualizar pessoa
    Refatorando para reutilizacao do codigo;

--- controladores/pessoa_controlador.py
+++ controladores/pessoa_controlador.py
@@ -4,6 +4,29 @@ from entidades import Pessoa
 
 pessoa_bp = Blueprint('pessoa_bp', __name__)
 
+def buscar_pessoa_por_id(session, id):
+    pessoa = session.query(Pessoa).get(id)
+
+    if pessoa is None:
+        return None, jsonify({'erros': ['Pessoa não encontrada!']}), 404
+    
+    return pessoa, None, None
+
+def validar(dados):
+    erros = []
+
+    nome = dados.get('nome')
+
+    if not nome:
+        erros.append('Nome é obrigatório!')
+
+    if erros:
+        return None, jsonify({'erros': erros }), 422
+    
+    pessoa = Pessoa(nome=nome)
+
+    return pessoa, None, None
+
 @pessoa_bp.route('/pessoas', methods=['GET'])
 def buscar_tudo():
     with session_scope() as session:
@@ -14,23 +37,21 @@ def buscar_tudo():
 @pessoa_bp.route('/pessoas/<int:id>', methods=['GET'])
 def buscar_por_id(id):
     with session_scope() as session:
-        pessoa = session.query(Pessoa).get(id)
+        pessoa, erro, status = buscar_pessoa_por_id(session, id)
 
-        if pessoa is None:
-            return jsonify({'mensagem': 'Pessoa não encontrada!'}), 404
-
+        if erro:
+            return erro, status
+        
         return jsonify(pessoa.to_dict()), 200
 
 @pessoa_bp.route('/pessoas', methods=['POST'])
 def criar():
     dados = request.get_json()
 
-    nome = dados.get('nome')
+    pessoa, erro, status = validar(dados)
 
-    if not nome:
-        return jsonify({'mensagem': 'Nome é obrigatório!'}), 422
-
-    pessoa = Pessoa(nome=nome)
+    if erro:
+        return erro, status
 
     with session_scope() as session:
         session.add(pessoa)
@@ -40,3 +61,22 @@ def criar():
         location_url = f"{request.host_url.rstrip('/')}{url_for('pessoa_bp.buscar_por_id', id=pessoa.id)}"
     
         return '', 201, {'Location': location_url}
+
+@pessoa_bp.route('/pessoas/<int:id>', methods=['PUT'])
+def atualizar(id):
+    dados = request.get_json()
+
+    pessoaNovosDados, erro, status = validar(dados)
+
+    if erro:
+        return erro, status
+
+    with session_scope() as session:
+        pessoa, erro, status = buscar_pessoa_por_id(session, id)
+        
+        if erro:
+            return erro, status
+
+        pessoa.nome = pessoaNovosDados.nome
+
+        return jsonify(pessoa.to_dict()), 200
