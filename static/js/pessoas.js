e07dc9730f77706f4cd8de55b8069d4f3fa4b671
MatheusFilipe21 <matheusfilipe1999@gmail.com>
2024-09-30 13:43:20 -0300

    Adicionando a atualizacao da pessoa

--- static/js/pessoas.js
+++ static/js/pessoas.js
@@ -22,6 +22,8 @@ function verificarListaDePessoas() {
 $("#pessoaModal").on("show.bs.modal", async function (event) {
   var button = $(event.relatedTarget);
   var acao = button.data("acao");
+  var id = button.data("id");
+  var nome = button.data("nome");
 
   var modal = $(this);
   var inputNome = modal.find("#nomePessoa");
@@ -31,6 +33,9 @@ $("#pessoaModal").on("show.bs.modal", async function (event) {
   switch (acao) {
     case "criar":
       prepararModalParaCriacao(modal, inputNome, salvarBtn, nomePessoaErro);
+      break;
+    case "editar":
+      prepararModalParaEdicao(modal, inputNome, salvarBtn,nomePessoaErro, id, nome);
       break;
   }
 });
@@ -63,6 +68,35 @@ function prepararModalParaCriacao(modal, inputNome, salvarBtn, nomePessoaErro) {
   });
 }
 
+function prepararModalParaEdicao(modal, inputNome, salvarBtn, nomePessoaErro, id, nome) {
+  modal.find(".modal-title").text("Editar Pessoa");
+  inputNome.val(nome).prop("disabled", false);
+  salvarBtn
+    .text("Salvar alterações")
+    .removeClass("btn-danger")
+    .addClass("btn-primary");
+
+    inputNome.off("input").on("input", function() {
+      nomePessoaErro.hide();
+    });
+
+  salvarBtn.off("click").on("click", async function () {
+    if (!validarNome(inputNome, nomePessoaErro)) {
+      return;
+    }
+
+    const novoNome = inputNome.val();
+
+    await atualizarPessoa(id, novoNome);
+
+    $(`#pessoa_${id} td:first`).text(novoNome);
+
+    $(`#pessoa_${id} [data-acao="editar"]`).data("nome", novoNome);
+
+    modal.modal("hide");
+  });
+}
+
 function validarNome(inputNome, nomePessoaErro) {
   if (!inputNome.val()) {
     nomePessoaErro.text("O nome não pode estar vazio.").show();
@@ -78,7 +112,13 @@ function validarNome(inputNome, nomePessoaErro) {
 function novaPessoaHtml(novaPessoa) {
   return `
 <tr id="pessoa_${novaPessoa.id}">
-  <td class="align-middle">${novaPessoa.nome}</td>
+  <td>${novaPessoa.nome}</td>
+
+  <td class="text-center">
+    <button class="btn btn-sm btn-warning" data-bs-toggle="modal" data-bs-target="#pessoaModal" data-id="${novaPessoa.id}" data-nome="${novaPessoa.nome}" data-acao="editar">
+      <i class="fa-solid fa-pen text-light"></i>
+    </button>
+  </td>
 </tr>
 `;
 }
@@ -124,5 +164,25 @@ async function buscarPessoaPorId(id) {
     return pessoa;
   } catch (error) {
     exibirNotificacao("Erro ao recuperar a pessoa.", true);
+  }
+}
+
+async function atualizarPessoa(id, nome) {
+  try {
+    const response = await fetch(`${urlApiPessoas}/${id}`, {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ nome: nome }),
+    });
+
+    if (!response.ok) {
+      throw new Error();
+    }
+
+    exibirNotificacao("Pessoa atualizada com sucesso!", false);
+  } catch (error) {
+    exibirNotificacao("Erro ao atualizar a pessoa.", true);
   }
 }
