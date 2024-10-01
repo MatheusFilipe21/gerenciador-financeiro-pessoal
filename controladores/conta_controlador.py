53e35ab108c1639f9257507ff245470079202ebb
MatheusFilipe21 <matheusfnpereira@gmail.com>
2024-10-01 11:00:16 -0300

    Criando endpoint para criar uma conta

--- controladores/conta_controlador.py
+++ controladores/conta_controlador.py
@@ -1,6 +1,7 @@
 from flask import Blueprint, jsonify, request, url_for
 from session_manager import session_scope
 from entidades import Conta
+from controladores.pessoa_controlador import buscar_pessoa_por_id
 
 conta_bp = Blueprint('conta_bp', __name__)
 
@@ -15,6 +16,33 @@ def buscar_conta_por_id(session, id):
     if conta is None:
         return None, jsonify({'erros': ['Conta não encontrada!']}), 404
     
+    return conta, None, None
+
+def validar(dados):
+    erros = []
+
+    nome = dados.get('nome')
+    pessoa_dados = dados.get('pessoa')
+
+    if not nome:
+        erros.append('Nome é obrigatório!')
+
+    if not pessoa_dados or not pessoa_dados.get('id'):
+        erros.append('Pessoa é obrigatória e deve conter um id!')
+
+    if erros:
+        return None, jsonify({'erros': erros }), 422
+
+    pessoa_id = pessoa_dados['id']
+
+    with session_scope() as session:
+        pessoa, erro, status = buscar_pessoa_por_id(session, pessoa_id)
+        
+        if erro:
+            return None, erro, status
+
+    conta = Conta(nome=nome, pessoa_id=pessoa_id)
+
     return conta, None, None
 
 @conta_bp.route('/contas', methods=['GET'])
@@ -32,3 +60,21 @@ def buscar_por_id(id):
             return erro, status
         
         return jsonify(conta.to_dict()), 200
+
+@conta_bp.route('/contas', methods=['POST'])
+def criar():
+    dados = request.get_json()
+
+    conta, erro, status = validar(dados)
+
+    if erro:
+        return erro, status
+
+    with session_scope() as session:
+        session.add(conta)
+        # TODO: Nao era para ser necessario esse commit, mas, so esta funcionando assim.
+        session.commit()
+
+        location_url = f"{request.host_url.rstrip('/')}{url_for('conta_bp.buscar_por_id', id=conta.id)}"
+    
+        return '', 201, {'Location': location_url}
